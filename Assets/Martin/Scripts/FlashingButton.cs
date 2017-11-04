2bf62300efdc5b5b1e291bf2cc967760a5aa8086
Martin <martybalch21@live.com>
2017-11-04 03:05:22 -0700

    worked on main menu and made the status UI prefabs

---
+++ Assets/Martin/Scripts/FlashingButton.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlashingButton : MonoBehaviour
+{
+    public Text enter;
+    bool On = false;
+    public Color Blink;
+    public Color noBlink;
+    public float startTime;
+    private float currentTime;
+
+    // Use this for initialization
+    void Start () {}
+
+	void OnOff()
+    {
+        if(On)
+        {
+            enter.color = Blink;
+            On = false;
+        }
+        else
+        {
+            enter.color = noBlink;
+            On = true;
+        }
+    }
+	// Update is called once per frame
+	void Update ()
+    {
+        currentTime -= Time.deltaTime;
+        if(currentTime < 0)
+        {
+            OnOff();
+            currentTime = startTime;
+        }		
+	}
+}
